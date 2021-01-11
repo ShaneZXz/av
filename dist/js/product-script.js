b2a0213fd96efa11c3a2bf8e158f09e27c90a5e3
ShaneZXz <shanezilber@gmail.com>
2021-01-12 00:26:11 +0200

    some more stuff

--- dist/js/product-script.js
+++ dist/js/product-script.js
@@ -1,5 +1,11 @@
 $('.av-btn-group').on('click','.av-btn',function(e){
     $(this).parent().find('.av-btn').removeClass('active');
     $(this).addClass('active');
+    if($(this).data('av-btn') === 'bp-plus'){
+        $('[data-bp-plus]').fadeIn('slow');
+    }
+    else if($(this).data('av-btn') === 'bp-reg'){
+        $('[data-bp-plus]').fadeOut('slow');
+    }
 });
 
